d40ef52dfd4079c89671279c7be323b9979f0553
SaFed23 <04.safed.04@gmail.com>
2019-11-14 18:04:29 +0300

    added file with array

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 string = input("Введите строку из 0 и 1: ")
-arr = []
-for char in string:
-    arr.append(input("Введите элемент: "))
+with open("./data.txt", "r") as file:
+    arr = file.readline().split(",")
+print("Начальный массив: ", arr)
 result_arr = list(filter(lambda element: string[arr.index(element)] == "1", arr))
 print("Результат:", result_arr)
